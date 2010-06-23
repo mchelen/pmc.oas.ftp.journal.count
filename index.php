8d3668b315c845f634f512bb76766f792f8685d3
Michael Chelen <osci1@ps15693.dreamhostps.com>
2010-06-22 18:55:10 -0700

    reformat to use functions

--- index.php
+++ index.php
@@ -7,29 +7,68 @@
  */
 // file url
 $url = "ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/file_list.txt";
-// xml file name
-$xmlfilename = "output.xml";
 // output path
 $outputpath = "output";
-// open remote file
-$handle = fopen($url, "r");
-if ($handle) {
-    // read first line
-    $firstline = fgets($handle);
-    fclose($handle);
-}
-// reformat first line for usage as timestamp
-$timestamp=trim($firstline);
-// generate file name based on timestamp
-$filename = "file_list.".preg_replace('/[^0-9]/',".",$timestamp).".txt";
+// output file name
+$outputfile = "output.xml";
+// source file path
+$sourcepath = "source";
+
+// get the timestamp from the remote file
+$timestamp = gettimestamp($url);
+
+// generate source file name based on timestamp
+$sourcefile = "$sourcepath/file_list.".preg_replace('/[^0-9]/',".",$timestamp).".txt";
+
 // check if that file has been downloaded yet
-if (!file_exists($filename)) {
-  // file does not exist, download file with wget
-  $output = `wget -O $filename $url`;
+if (!file_exists($sourcefile)) {
+  // source file does not exist
+  // download file
+  downloadfile($sourcepath, $sourcefile, $url);
+
+  // get journal counts from source file
+  echo "getting journal counts from $sourcefile \n";
+  $result = getjournalcounts($sourcefile);
+
+  // generate xml from source file
+  makexml($sourcefile, $outputfile, $result["journals"], $result["timestamp"]);
+}
+// current souce file already exists
+else {
+  print "$sourcefile already exists, xml file untouched <a href=\"$outputfile\">$outputfile</a>";
+}
+
+function gettimestamp($url) {
+  // gets timestamp from first line of remote file
+  // open remote file
+  $handle = fopen($url, "r");
+  if ($handle) {
+      // read first line
+      $firstline = fgets($handle);
+      fclose($handle);
+  }
+  // trim first line for usage as timestamp
+  $timestamp=trim($firstline);
+  // return trimmed timestamp
+  return $timestamp;
+}
+function downloadfile($sourcepath, $sourcefile, $url) {
+  // file does not exist
+  // create source file directory
+  $output = `mkdir -p $sourcepath`;
+  // download file with wget
+  $output = `wget -O $sourcefile $url`;
+}
+
+function getjournalcounts($sourcefile) {
+  // get journal counts from source file
   // open local file
-  $handle = fopen($filename, "r");
-  // skip first line
-  fgets($handle);
+  $handle = fopen($sourcefile, "r");
+  // read timestamp
+  if ($handle) {
+    $timestamp = fgets($handle);
+  }
+  
   // initialize journal array
   $journals = array();
   // read file as csv (tsv)
@@ -49,34 +88,37 @@ if (!file_exists($filename)) {
   }
   // close file
   fclose($handle);
+  $result["journals"] = $journals;
+  $result["timestamp"] = $timestamp;
+  return $result;
+}
+function makexml($sourcefile,$outputfile, $journals, $timestamp) {
   // create dom document
   $doc = new DOMDocument('1.0', 'UTF-8');
-  // create root
+  // create root element
   $root = $doc->createElement("root");
-  // attach root
+  // attach root element to document
   $doc->appendChild($root);
-  // create recordset
+  // create recordset element
   $recordset = $doc->createElement("dataset");
-  // append recordset to root
+  // append recordset element to root
   $root->appendChild($recordset);
   // create date attribute
   $attr_date = $doc->createAttribute('date');
-  // append attribute to row
+  // append attribute to recordset element
   $recordset->appendChild($attr_date);
   // create date node
   $date = $doc->createTextNode($timestamp);
   // append date to attribute
   $attr_date->appendChild($date);
   foreach ($journals as $key => $value) {
-  //  echo "$key = $value, ";
-      
-  //  create row
+  //  create row element
       $row = $doc->createElement("journal",$value);
-  //  append row to recordset
+  //  append row to recordset element
       $recordset->appendChild($row);
   //  create title attribute
       $attr = $doc->createAttribute('title');
-  //  append attribute to row
+  //  append attribute to row element
       $row->appendChild($attr);
   //  create title node
       $title = $doc->createTextNode($key);
@@ -85,44 +127,38 @@ if (!file_exists($filename)) {
   }
 
   // load xml file if it exists
-  if (file_exists($xmlfilename)) {
+  if (file_exists($outputfile)) {
+    appendxml($outputfile, $recordset);
+  }
+  else {
+  // xml output file does not exist
+    writenewxml($doc, outputfile);
+  }
+}
+function appendxml($outputfile, $recordset) {
   //  load xml
     $olddoc = new DOMDocument();
-    $olddoc->load($xmlfilename);
-  //  apply xpath to find date
-    $xpath = new DOMXpath($olddoc);
-    $elements = $xpath->query("//dataset/@date");
-    if (!is_null($elements)) {
-      foreach ($elements as $element) {
-          $lastdate = $element->nodeValue;
-      }
-    }
-  //    import new recordset to old xml
-      $node = $olddoc->importNode($recordset, true);
-  //    make it pretty
-      $node->formatOutput=true;
-      $olddoc->documentElement->appendChild($node);
-  //    make it pretty
-      $olddoc->formatOutput=true;
-  //    write XML file
-      $fh = fopen($xmlfilename, 'w') or die("can't open file");
-      fwrite($fh, $olddoc->saveXML());
-      fclose($fh);
-      print "appended data to xml file <a href=\"$xmlfilename\">$xmlfilename</a>";
-    }
-  
-  // xml output file does not exist
-  else {
+    $olddoc->load($outputfile);
+//    import new recordset to old xml
+    $node = $olddoc->importNode($recordset, true);
+//    make it pretty
+    $node->formatOutput=true;
+    $olddoc->documentElement->appendChild($node);
+//    make it pretty
+    $olddoc->formatOutput=true;
+//    write XML file
+    $fh = fopen($outputfile, 'w') or die("can't open file");
+    fwrite($fh, $olddoc->saveXML());
+    fclose($fh);
+    print "appended data to xml file <a href=\"$outputfile\">$outputfile</a>";
+}
+function writenewxml($doc, $outputfile) {
   //  make it pretty
     $doc->formatOutput=true;
   //  write XML file
-    $fh = fopen($xmlfilename, 'w') or die("can't open file");
+    $fh = fopen($outputfile, 'w') or die("can't open file");
     fwrite($fh, $doc->saveXML());
     fclose($fh);
-    print "wrote new xml file <a href=\"$xmlfilename\">$xmlfilename</a>";
-  }
-}
-else {
-  print "$filename already exists, xml file untouched <a href=\"$xmlfilename\">$xmlfilename</a>";
+    print "wrote new xml file <a href=\"$outputfile\">$outputfile</a>";
 }
 ?>
