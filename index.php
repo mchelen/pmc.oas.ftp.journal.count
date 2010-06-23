fa38062eed27de3d52970c5d8ad78c6fa209567f
Michael Chelen <osci1@ps15693.dreamhostps.com>
2010-06-22 21:34:33 -0700

    lots of updates

--- index.php
+++ index.php
@@ -1,42 +1,60 @@
 <?php
 /**
  * author: Michael Chelen http://mikechelen.com http://twitter.com/mikechelen
- * license: Creative Commons Zero
+ * license: Creative Commons Zero http://creativecommons.org/publicdomain/zero/1.0/
  * downloads pmc open access subset ftp file list and computes article counts for each journal
  * source: ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/file_list.txt
  */
 // file url
 $url = "ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/file_list.txt";
-// output path
+// output file path
 $outputpath = "output";
 // output file name
-$outputfile = "output.xml";
+$outputfile = "$outputpath/output.xml";
 // source file path
 $sourcepath = "source";
-
 // get the timestamp from the remote file
 $timestamp = gettimestamp($url);
-
 // generate source file name based on timestamp
 $sourcefile = "$sourcepath/file_list.".preg_replace('/[^0-9]/',".",$timestamp).".txt";
-
 // check if that file has been downloaded yet
 if (!file_exists($sourcefile)) {
   // source file does not exist
   // download file
   downloadfile($sourcepath, $sourcefile, $url);
 
-  // get journal counts from source file
-  echo "getting journal counts from $sourcefile \n";
-  $result = getjournalcounts($sourcefile);
+  dostuff($sourcefile,$outputfile,$outputpath);
 
-  // generate xml from source file
-  makexml($sourcefile, $outputfile, $result["journals"], $result["timestamp"]);
 }
-// current souce file already exists
+// current source file already exists
 else {
-  print "$sourcefile already exists, xml file untouched <a href=\"$outputfile\">$outputfile</a>";
+  print "<br />\n$sourcefile already exists, xml file untouched <a href=\"$outputfile\">$outputfile</a>";
 }
+
+function dostuff($sourcefile,$outputfile,$outputpath){
+  // get journal counts from source file
+  echo "<br />\nGetting journal counts from $sourcefile";
+  $journalcounts = getjournalcounts($sourcefile);
+  // generate xml
+  $xml = makexml($journalcounts["journals"], $journalcounts["timestamp"]);
+  $doc = $xml["doc"];
+  $recordset = $xml["recordset"];
+  // load xml file if it exists
+  if (file_exists($outputfile)) {
+    // append new xml to existing
+    print "<br />\nAppending data to xml file <a href=\"$outputfile\">$outputfile</a>";
+    appendxml($outputfile, $recordset);
+  }
+  else {
+    print "<br />\nWriting new xml file <a href=\"$outputfile\">$outputfile</a>";
+    // xml output file does not exist
+    $output = `mkdir -p $outputpath`;
+    writenewxml($doc, $outputfile);
+  }
+ 
+}
+
+
 
 function gettimestamp($url) {
   // gets timestamp from first line of remote file
@@ -59,30 +77,28 @@ function downloadfile($sourcepath, $sourcefile, $url) {
   // download file with wget
   $output = `wget -O $sourcefile $url`;
 }
-
 function getjournalcounts($sourcefile) {
   // get journal counts from source file
   // open local file
   $handle = fopen($sourcefile, "r");
   // read timestamp
   if ($handle) {
-    $timestamp = fgets($handle);
+    $timestamp = trim(fgets($handle));
   }
-  
   // initialize journal array
   $journals = array();
   // read file as csv (tsv)
   while (($data = fgetcsv($handle, 0, chr(9))) !== FALSE) {
-  // store journal name
+    // store journal name
     preg_match('/^[^\.]+/', $data[1],$matches);
     $key = $matches[0];
-  // check if journal count exists
+    // check if journal count exists
     if (array_key_exists($key,$journals)) {
-  //  increment journal count
+      // increment journal count
       $journals[$key]=$journals[$key]+1;
     }
     else {
-  //  start new journal count
+      // start new journal count
       $journals[$key]=1;
     }
   }
@@ -92,7 +108,7 @@ function getjournalcounts($sourcefile) {
   $result["timestamp"] = $timestamp;
   return $result;
 }
-function makexml($sourcefile,$outputfile, $journals, $timestamp) {
+function makexml($journals, $timestamp) {
   // create dom document
   $doc = new DOMDocument('1.0', 'UTF-8');
   // create root element
@@ -125,40 +141,32 @@ function makexml($sourcefile,$outputfile, $journals, $timestamp) {
   //  append title to attribute
       $attr->appendChild($title);
   }
-
-  // load xml file if it exists
-  if (file_exists($outputfile)) {
-    appendxml($outputfile, $recordset);
-  }
-  else {
-  // xml output file does not exist
-    writenewxml($doc, outputfile);
-  }
+  $result["doc"]=$doc;
+  $result["recordset"]=$recordset;
+  return $result;
 }
 function appendxml($outputfile, $recordset) {
-  //  load xml
+    // load xml
     $olddoc = new DOMDocument();
     $olddoc->load($outputfile);
-//    import new recordset to old xml
+    // import new recordset to old xml
     $node = $olddoc->importNode($recordset, true);
-//    make it pretty
+    // make it pretty
     $node->formatOutput=true;
     $olddoc->documentElement->appendChild($node);
-//    make it pretty
+    // make it pretty
     $olddoc->formatOutput=true;
-//    write XML file
+    // write XML file
     $fh = fopen($outputfile, 'w') or die("can't open file");
     fwrite($fh, $olddoc->saveXML());
     fclose($fh);
-    print "appended data to xml file <a href=\"$outputfile\">$outputfile</a>";
 }
 function writenewxml($doc, $outputfile) {
-  //  make it pretty
+    // make it pretty
     $doc->formatOutput=true;
-  //  write XML file
+    // write XML file
     $fh = fopen($outputfile, 'w') or die("can't open file");
     fwrite($fh, $doc->saveXML());
     fclose($fh);
-    print "wrote new xml file <a href=\"$outputfile\">$outputfile</a>";
 }
 ?>
